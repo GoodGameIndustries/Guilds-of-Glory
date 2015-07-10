c5e9d9b5d6bcaf547fe010a989efc3339b80d744
EmmettGGI <goodgameindustries@gmail.com>
2015-07-10 01:27:30 -0400

    LibGDX Setup

---
+++ android/src/com/GGI/GoG/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.GGI.GoG.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.GGI.GoG.GoG;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GoG(), config);
+	}
+}
---
+++ desktop/src/com/GGI/GoG/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.GGI.GoG.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.GGI.GoG.GoG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoG(), config);
+	}
+}
